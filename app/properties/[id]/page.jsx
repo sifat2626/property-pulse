d1739a9742838f1c79b521d21068a07b7a1f82f7
sifat2626 <ashrafulsifat26@gmail.com>
2024-10-29 12:51:00 +0600

    navbar

---
+++ app/properties/[id]/page.jsx
@@ -0,0 +1,7 @@
+"use client";
+
+function page() {
+  return <div>id</div>;
+}
+
+export default page;
---
+++ app/properties/page.jsx
@@ -0,0 +1,12 @@
+import Link from "next/link";
+
+function page() {
+  return (
+    <div>
+      <h3>Properties</h3>
+      <Link href={"/"}>Go Home</Link>
+    </div>
+  );
+}
+
+export default page;
