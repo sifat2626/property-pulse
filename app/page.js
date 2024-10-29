dcdfa7ec0ff9bf0c88a54cb338e4bb19f8852974
sifat2626 <ashrafulsifat26@gmail.com>
2024-10-29 13:20:05 +0600

    hero, footer

--- app/page.js
+++ app/page.js
@@ -1,9 +1,11 @@
 import Hero from "@/components/Hero";
+import InfoBoxes from "@/components/InfoBoxes";
 
 export default function Home() {
   return (
     <div className="">
       <Hero />
+      <InfoBoxes />
     </div>
   );
 }
